6df76e7e812919dd1119d504d2ff21b21cadae8e
natalyst <nattapong.anuwong@gmail.com>
2023-02-02 09:49:14 +0700

    feat: implement todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+type Todo = {
+  id: Number;
+  title: String;
+  done?: Boolean;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'post-app';
+  todoInput = '';
+  todos: Todo[] = [];
+
+  getTodos() {
+    const todosInLocalStorage = localStorage.getItem('todos');
+    if (todosInLocalStorage) {
+      const todosFormattedData = JSON.parse(todosInLocalStorage);
+      console.log({ todosFormattedData });
+
+      this.todos = todosFormattedData;
+    }
+  }
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  storeTodos(todo: Todo) {
+    const todosInLocalStorage = localStorage.getItem('todos');
+
+    if (todosInLocalStorage === null) {
+      localStorage.setItem('todos', JSON.stringify([todo]));
+    } else {
+      let todosFormattedData = JSON.parse(todosInLocalStorage) as Todo[];
+      todosFormattedData = [...todosFormattedData, todo];
+      localStorage.setItem('todos', JSON.stringify(todosFormattedData));
+    }
+  }
+
+  addTodo() {
+    const todoTemp = {
+      title: this.todoInput,
+      id: Date.now(),
+    };
+    this.storeTodos(todoTemp);
+    this.todos = [...this.todos, todoTemp];
+    this.todoInput = '';
+  }
 }
